316e8a0b24466c03b1c83d5cb95fa95f6d365a3d
LekcRg <lekcrg@ya.ru>
2017-10-20 22:04:30 +0300

    version 2.0

--- bars.py
+++ bars.py
@@ -4,62 +4,36 @@ from math import sqrt
 
 
 def load_json(path_to_file):
-    with open(path_to_file, "r") as file:
+    with open(path_to_file, "r", encoding='utf-8') as file:
         return json.load(file)
 
 
 def get_biggest_bar(json_data):
-    seats_count = []
-
-    for bars in json_data['features']:
-        if bars['properties']['Attributes']['SeatsCount'] > 0:
-            seats_count.append(bars['properties']['Attributes']['SeatsCount'])
-
-    biggest_bar = max(seats_count)
-
-    for bars in json_data['features']:
-        if bars['properties']['Attributes']['SeatsCount'] == biggest_bar:
-            print("Name of bar: " + bars['properties']['Attributes']['Name'] + "\n" +
-                  "Address: " + bars['properties']['Attributes']['Address'])
+    return max(json_data, key=lambda bar: bar['properties']['Attributes']['SeatsCount'])
 
 
 def get_smallest_bar(json_data):
-    seats_count = []
-
-    for bars in json_data['features']:
-        if bars['properties']['Attributes']['SeatsCount'] > 0:
-            seats_count.append(bars['properties']['Attributes']['SeatsCount'])
-
-    smallest_bar = min(seats_count)
-
-    for bars in json_data['features']:
-        if bars['properties']['Attributes']['SeatsCount'] == smallest_bar:
-            print("Name of bar: " + bars['properties']['Attributes']['Name'] + "\n" +
-                  "Address: " + bars['properties']['Attributes']['Address'])
+    return min(json_data, key=lambda bar: bar['properties']['Attributes']['SeatsCount'])
 
 
 def get_closest_bar(json_data, longitude, latitude):
-    distance = []
-    bar_number = 0
-
-    for bar in json_data['features']:
-        distance.append(sqrt((bar['geometry']['coordinates'][0] - latitude) ** 2 +
-                             (bar['geometry']['coordinates'][1] - longitude) ** 2))
-
-    for bar_distance in distance:
-        if bar_distance == min(distance) and bar_number != 150:
-            print("Name of bar: " + json_data['features'][bar_number]['properties']['Attributes']['Name'] + "\n" +
-                  "Address: " + json_data['features'][bar_number]['properties']['Attributes']['Address'])
-        bar_number += 1
+    return min(json_data, key=lambda bar: sqrt((bar['geometry']['coordinates'][0] - latitude) ** 2 +
+                                               (bar['geometry']['coordinates'][1] - longitude) ** 2))
 
 
-bars_info = load_json("bars_info.json")
 if __name__ == '__main__':
-    if sys.argv[1] == "get_biggest_bar":
-        get_biggest_bar(bars_info)
-
-    if sys.argv[1] == "get_smallest_bar":
-        get_smallest_bar(bars_info)
-
-    if sys.argv[1] == "get_closest_bar":
-        get_closest_bar(bars_info, float(sys.argv[2]), float(sys.argv[3]))
+    bars_info = load_json(sys.argv[1])['features']
+    if sys.argv[2] == "biggest_bar":
+        print("Самый большой бар\nНазвание: {name}\nАдрес: {address}\n"
+              .format(name=get_biggest_bar(bars_info)['properties']['Attributes']['Name'],
+                      address=get_biggest_bar(bars_info)['properties']['Attributes']['Address']))
+    if sys.argv[2] == "smallest_bar":
+        print("Самый маленький бар\nНазвание: {name}\nАдрес: {address}\n"
+              .format(name=get_smallest_bar(bars_info)['properties']['Attributes']['Name'],
+                      address=get_smallest_bar(bars_info)['properties']['Attributes']['Address']))
+    if sys.argv[2] == "closest_bar":
+        print("Самый близкий бар\nНазвание: {name}\nАдрес: {address}\n"
+              .format(name=get_closest_bar(bars_info, float(sys.argv[3]),
+                                           float(sys.argv[4]))['properties']['Attributes']['Name'],
+                      address=get_closest_bar(bars_info, float(sys.argv[3]),
+                                              float(sys.argv[4]))['properties']['Attributes']['Address']))
